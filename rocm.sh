21c934fe6e56a6c3c2e07ecd47a0984063194b1d
Daniel Tang <danielzgtg.opensource@gmail.com>
2023-02-27 02:27:36 -0500

    Install keybert

--- rocm.sh
+++ rocm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 . venv/bin/activate
-pip3 install torch --extra-index-url https://download.pytorch.org/whl/rocm5.2/
+pip3 install torch --force --extra-index-url https://download.pytorch.org/whl/rocm5.2/
